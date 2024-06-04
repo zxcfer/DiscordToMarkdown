80cc117a4a90115bbd472db8fe8abfe288c05e39
zxcfer <zxcfer@proton.me>
2024-06-04 07:07:22 +0200

    first commit

---
+++ lib/discord2markdown.ex
@@ -0,0 +1,18 @@
+defmodule Discord2markdown do
+  @moduledoc """
+  Documentation for `Discord2markdown`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Discord2markdown.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/discord2markdown_test.exs
@@ -0,0 +1,8 @@
+defmodule Discord2markdownTest do
+  use ExUnit.Case
+  doctest Discord2markdown
+
+  test "greets the world" do
+    assert Discord2markdown.hello() == :world
+  end
+end
